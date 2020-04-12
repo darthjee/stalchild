0692a7f93e6dc902ecb91e90abbd124f76325f66
darthjee <darthjee@gmail.com>
2020-04-12 19:25:33 +0200

    Add taa project

---
+++ taa/source/app/assets/javascripts/modules/routes.js
@@ -0,0 +1,19 @@
+(function(angular) {
+  var module = angular.module('stalchild');
+
+  module.config(['kantoProvider', function(provider) {
+    provider.defaultConfig = {
+      controller: 'Cyberhawk.Controller',
+      controllerAs: 'gnc',
+      templateBuilder: function(route, params) {
+        return route + '?ajax=true';
+      }
+    }
+
+    provider.configs = [{
+      routes: []
+    }];
+    provider.$get().bindRoutes();
+  }]);
+}(window.angular));
+
