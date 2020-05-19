4417467b9a9a90a26bde2fb5a1a9e0922db58b16
Favini <darthjee@gmail.com>
2020-05-19 21:40:25 +0200

    Taa layout (#7)

--- taa/source/app/helpers/form_helper.rb
+++ taa/source/app/helpers/form_helper.rb
@@ -1,29 +1,11 @@
 # frozen_string_literal: true
 
 module FormHelper
-  def bootstrap_input(model, field, placeholder: nil, label: field.capitalize)
-    locals = {
-      label: label,
-      ng_errors: [model, :errors, field].join('.'),
-      ng_model: [model, field].join('.'),
-      model: model,
-      field: field,
-      placeholder: placeholder
-    }
-
-    render partial: 'templates/forms/input', locals: locals
+  def bootstrap_form(model)
+    yield Form.new(self, model)
   end
 
-  def bootstrap_select(model, field, label: field.capitalize, options: [])
-    locals = {
-      label: label,
-      ng_errors: [model, :errors, field].join('.'),
-      ng_model: [model, field].join('.'),
-      model: model,
-      field: field,
-      options: options
-    }
-
-    render partial: 'templates/forms/select', locals: locals
+  def bootstrap_display(model)
+    yield Display.new(self, model)
   end
 end
---
+++ taa/source/app/helpers/form_helper/input.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module FormHelper
+  class Input < InputElement
+    private
+
+    attr_reader :placeholder
+    default_value :template, 'templates/forms/input'
+
+    def initialize(placeholder: nil, **args)
+      super(**args)
+      @placeholder = placeholder
+    end
+
+    def locals
+      super.merge(placeholder: placeholder)
+    end
+  end
+end
