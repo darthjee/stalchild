8112d8afa09e6923a68b10e3b615f8152e521e1a
Favini <darthjee@gmail.com>
2020-06-04 15:57:43 +0200

    Rails gem (#8)

---
+++ rails_gem/spec/spec_helper.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+
+SimpleCov.profiles.define 'gem' do
+  add_filter '/spec/'
+end
+
+SimpleCov.start 'gem'
+
+require 'stalchild'
+require 'pry-nav'
+
+require 'active_record'
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3', database: ':memory:'
+)
+
+require File.expand_path('spec/dummy/config/environment')
+require File.expand_path('spec/dummy/db/schema.rb')
+require 'rspec/rails'
+require 'active_support/railtie'
+
+support_files = File.expand_path('spec/support/**/*.rb')
+Dir[support_files].sort.each { |file| require file }
+
+RSpec::Matchers.define_negated_matcher :not_change, :change
+RSpec::Matchers.define_negated_matcher :not_add_method, :add_method
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.filter_run_excluding :integration unless ENV['ALL']
+
+  config.order = 'random'
+end
