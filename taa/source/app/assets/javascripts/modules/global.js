087b6292c85b51f7160dddfaa00a4aa20d29a3c6
darthjee <darthjee@gmail.com>
2022-04-05 18:12:23 +0200

    Update taa based on taau

--- taa/source/app/assets/javascripts/modules/global.js
+++ taa/source/app/assets/javascripts/modules/global.js
@@ -1,6 +1,8 @@
 (function(angular) {
   window.Global = {};
 
-  angular.module('global', [
+  angular.module("global", [
+    "global/controller",
+    "global/header_controller"
   ])
 }(window.angular));
