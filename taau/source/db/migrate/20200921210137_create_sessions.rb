bac5a78e7fd622e8457c25c4714b8f85b871a620
darthjee <darthjee@gmail.com>
2022-05-14 22:27:21 +0200

    Update session token migration

--- taau/source/db/migrate/20200921210137_create_sessions.rb
+++ taau/source/db/migrate/20200921210137_create_sessions.rb
@@ -5,6 +5,9 @@ class CreateSessions < ActiveRecord::Migration[5.2]
     create_table :sessions do |t|
       t.bigint :user_id, null: false
       t.datetime :expiration
+      t.string :token, limit: 64, null: false
+
+      t.index :token, unique: true
     end
 
     add_foreign_key :sessions, :users
