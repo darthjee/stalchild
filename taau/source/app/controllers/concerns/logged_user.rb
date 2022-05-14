d87ef2e39e72feb19ecb806d30fa557a2d496fe5
darthjee <darthjee@gmail.com>
2022-05-14 22:41:34 +0200

    Fix mc doodle name space

--- taau/source/app/controllers/concerns/logged_user.rb
+++ taau/source/app/controllers/concerns/logged_user.rb
@@ -4,8 +4,8 @@ module LoggedUser
   extend ActiveSupport::Concern
 
   included do
-    rescue_from McDoodle::Exception::LoginFailed, with: :not_found
-    rescue_from McDoodle::Exception::NotLogged,   with: :not_found
+    rescue_from Stalchild::Exception::LoginFailed, with: :not_found
+    rescue_from Stalchild::Exception::NotLogged,   with: :not_found
   end
 
   private
@@ -25,6 +25,6 @@ module LoggedUser
   end
 
   def check_logged!
-    raise McDoodle::Exception::NotLogged unless logged_user
+    raise Stalchild::Exception::NotLogged unless logged_user
   end
 end
