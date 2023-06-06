7486fd88981b64eb73f54fa8f41f0dad6d01eea7
darthjee <darthjee@gmail.com>
2023-06-06 18:27:43 -0300

    Add new taau

---
+++ taau/source/app/models/settings.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Settings
+  extend Sinclair::EnvSettable
+
+  settings_prefix 'STALCHILD'
+
+  with_settings(
+    :password_salt,
+    hex_code_size: 16,
+    session_period: 2.days,
+    cache_age: 10.seconds,
+    title: 'Stalchild',
+    favicon: '/favicon.ico'
+  )
+end
