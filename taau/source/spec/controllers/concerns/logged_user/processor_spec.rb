5b9c7fdd400e48a0d5f7ba4e7068a3c138883657
darthjee <darthjee@gmail.com>
2024-04-23 07:17:17 -0300

    Fix spec

--- taau/source/spec/controllers/concerns/logged_user/processor_spec.rb
+++ taau/source/spec/controllers/concerns/logged_user/processor_spec.rb
@@ -50,11 +50,11 @@ describe LoggedUser::Processor do
       end
 
       it 'creates a session with expiration date' do
-        expect(session.expiration).to be <= Settings.session_period.from_now
-      end
-
-      it 'creates a session with expiration date in the future' do
-        expect(session.expiration).to be >= (Settings.session_period.from_now - 2.second)
+        expect(session.expiration)
+          .to be_between(
+            (Settings.session_period.from_now - 1.second),
+            Settings.session_period.from_now
+          )
       end
     end
   end
