b201ce48f70c93f77f03841d1fa28c7792169da6
darthjee <darthjee@gmail.com>
2024-06-19 08:26:10 -0300

    Fix render script

--- taa/scripts/deploy.sh
+++ taa/scripts/deploy.sh
@@ -52,7 +52,7 @@ function watch_deployment() {
     sleep $WAIT_TIME
 
     if [ "$WAIT_TIME" -gt 10 ]; then
-      WAIT_TIME=$[$WAIT_TIME/2]
+      WAIT_TIME=$[WAIT_TIME/2]
     fi
   done
 
@@ -60,11 +60,15 @@ function watch_deployment() {
   exit 1
 }
 
-function run_deploy() {
-  checkLastVersion
+function force_deploy() {
   SERVICE_ID=$(service_id)
   DEPLOYMENT_ID=$(deploy "$SERVICE_ID" | jq '.id' | sed -e 's/"//g')
   watch_deployment "$SERVICE_ID" "$DEPLOYMENT_ID"
+}
+
+function run_deploy() {
+  checkLastVersion
+  force_deploy
 }
 
 function watch_last_deployment() {
@@ -79,6 +83,9 @@ ACTION=$1
 case $ACTION in
   "deploy")
     run_deploy
+    ;;
+  "force_deploy")
+    force_deploy
     ;;
   "watch")
     watch_last_deployment
