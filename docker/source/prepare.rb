78be5a7669fa004f630ac4742555e3ae1471bc06
darthjee <darthjee@gmail.com>
2024-06-19 11:28:51 -0300

    Build image

--- docker/source/prepare.rb
+++ docker/source/prepare.rb
@@ -6,18 +6,16 @@ class String
   end
 end
 
-def prepare(type, project_name, base='stalchild')
-  system("cp -R #{type} #{project_name}")
-
-  system("sed -i -e 's/#{base}/#{project_name}/g' $(find #{project_name} -type f)")
-  system("sed -i -e 's/#{base.upcase}/#{project_name.upcase}/g' $(find #{project_name} -type f)")
-  system("sed -i -e 's/#{base.camel}/#{project_name.camel}/g' $(find #{project_name} -type f)")
-  system("rename s/#{base}/#{project_name}/ $(find #{project_name} -iname '*#{base}*')")
+def prepare(project_name, base='stalchild')
+  system("sed -i -e 's/#{base}/#{project_name}/g' $(find project/ -type f)")
+  system("sed -i -e 's/#{base.upcase}/#{project_name.upcase}/g' $(find project/ -type f)")
+  system("sed -i -e 's/#{base.camel}/#{project_name.camel}/g' $(find project/ -type f)")
+  system("rename s/#{base}/#{project_name}/ $(find project/ -iname '*#{base}*')")
 end
 
 begin
   prepare *ARGV
 rescue ArgumentError
-  puts "Usage:\nprepare.rb project_type new_project_name"
-  puts "Eg.\nprepare.rb taa my_awesome_project"
+  puts "Usage:\nprepare.rb new_project_name"
+  puts "Eg.\nprepare.rb my_awesome_project"
 end
