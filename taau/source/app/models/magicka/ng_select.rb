4aa9a71ecc942c6440ee9d5e4c46c01501ff550f
darthjee <darthjee@gmail.com>
2025-05-17 19:25:38 -0300

    Add custom ng_errors

--- taau/source/app/models/magicka/ng_select.rb
+++ taau/source/app/models/magicka/ng_select.rb
@@ -4,6 +4,10 @@ module Magicka
   # Element representing a special select that references
   # another list
   class NgSelect < Magicka::Select
-    with_attribute_locals :text_field, :reference_key
+    with_attribute_locals :text_field, :reference_key, :ng_errors
+
+    def ng_errors
+      @ng_errors ||= super
+    end
   end
 end
